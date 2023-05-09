9168e07f8cb1511dcdc501617f3ff0c5c04ea234
abdullah-aladham <abdullah.aladham@gmail.com>
2023-05-09 15:41:52 +0300

    adding packages sliders in react

--- kalemati/src/main/java/com/abdullah_aladham/kalemati/View/kalemati-react/src/App.js
+++ kalemati/src/main/java/com/abdullah_aladham/kalemati/View/kalemati-react/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import './App.css';
+//import kalemati from './kalemati';
+import Recomended from './recomended';
+import Kalemati from './kalemati';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <> 
+    
+    <h1 style={{textAlign: 'center',color: '#F6B968'}}>Kalemati | كـلماتي</h1>
+       <h2  style={{textAlign: 'center',color: '#F6B968'}}>My Packages</h2>
+       <Kalemati />
+       <h2 style={{textAlign: 'center',color: '#F6B968'}}>Recomended Packages</h2>
+       <Recomended />
+       </>
   );
 }
 
